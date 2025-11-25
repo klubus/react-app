2236202202a4828e2ce2a995fa3b7b27940fd00a
Krystian Kluba <klubakrystian@gmail.com>
2025-11-25 15:30:13 +0100

    redux modifications

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,7 +4,9 @@ import CardForm from '../CardForm/CardForm';
 import { useSelector } from 'react-redux';
 
 const Column = (props) => {
-  const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
+  const cards = useSelector((state) =>
+    state.cards.filter((card) => card.columnId === props.id)
+  );
 
   return (
     <article className={styles.column}>
@@ -17,7 +19,7 @@ const Column = (props) => {
           <Card key={card.id} title={card.title} />
         ))}
       </ul>
-      <CardForm columnId={props.id} action={props.addCard} />
+      <CardForm columnId={props.id} />
     </article>
   );
 };
